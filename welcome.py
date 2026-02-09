5572d6ab6ff30746e3bb7b6b6aec5feeb0bfa81f
viktoras-mayberry <sikiruolusegun528@gmail.com>
2026-02-09 13:54:54 +0100

    Remove the welcome.py file

--- welcome.py
+++
@@ -1,8 +0,0 @@
-def addition(num1, num2):
-    return num1 + num2
-
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-
-result = addition(num1, num2)
-print(f"The sum of {num1} and {num2} is {result}.")
